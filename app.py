1fd62fb71cc26d83a0b2ebfaeb00b7bcf4d98165
root <myke-fernandes@hotmail.com>
2020-10-27 01:42:34 +0000

    ac4

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+
+nmax = 100
+
+    n1 = 0
+    n2 = 1
+    cont = 0
+    fib = 0
+
+    fib = ("Esses são os 100 primeiros numeros da razão Fibonacci: <br> <br>")
+
+    while cont < nmax:
+        fib = n1 + n2
+        n1 = n2
+        n2 = fib
+        cont = cont + 1
+    return fib
+
+if __name__ == "__main__":
+      port = int(os.environ.get("port", 5000))
+      app.run(host='0.0.0.0', port=port)
